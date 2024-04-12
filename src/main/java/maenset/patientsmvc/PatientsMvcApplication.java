bb638e7a5985fb188d89e894027fdd994581a748
wiam <wiammiftah01@gmail.com>
2024-04-12 18:01:59 +0000

    pagination/mapping

--- src/main/java/maenset/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/maenset/patientsmvc/PatientsMvcApplication.java
@@ -1,13 +1,34 @@
 package maenset.patientsmvc;
 
+import maenset.patientsmvc.entities.Patient;
+import maenset.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class PatientsMvcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient(null, "Hassan",new Date(),false,12));
+            patientRepository.save(new Patient(null, "Wiam",new Date(),true,321));
+            patientRepository.save(new Patient(null, "Walid",new Date(),false,78));
+            patientRepository.save(new Patient(null, "Hanae",new Date(),true,20));
 
+            patientRepository.findAll().forEach(p -> {
+                System.out.println(p.getNom());
+            });
+
+
+        };
+    }
 }
---
+++ src/main/java/maenset/patientsmvc/web/PatientController.java
@@ -0,0 +1,28 @@
+package maenset.patientsmvc.web;
+
+
+import lombok.AllArgsConstructor;
+import maenset.patientsmvc.entities.Patient;
+import maenset.patientsmvc.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+    @GetMapping(path = "/index")
+    public String patients(Model model,
+                           @RequestParam(name = "page",defaultValue = "0") int page,
+                           @RequestParam(name = "size",defaultValue = "5") int size){
+        Page<Patient> patientPage=patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients",patientPage.getContent());
+        return "patients";
+    }
+}
